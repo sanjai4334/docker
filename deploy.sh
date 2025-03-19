11120f10fe90642d2b5325f27a217750a5acda8d
SANJAI S <codersanjai@gmail.com>
2025-03-19 04:50:28 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Set Docker image name, tag, and container name
-IMAGE_NAME="nadinc/docker"
+IMAGE_NAME="sanjai4334/docker"
 TAG="latest"
-CONTAINER_NAME="jenkins-docker-container"
+CONTAINER_NAME="my-container"
 
 # Stop and remove any existing container with the same name
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
 # Run the new Docker container
-docker run -d -p 8080:80 --name $CONTAINER_NAME $IMAGE_NAME:$TAG
+docker run -d -p 3001:80 --name $CONTAINER_NAME $IMAGE_NAME:$TAG
